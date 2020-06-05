21556e5eae4d8ea597d4acae0b2d25e1fc3b540b
Lisandro Mayancela <lmayancela08@gmail.com>
2020-06-05 16:52:14 -0400

    Add introduction paragraphs as well as functions to handle changing pages. Also add an images folder.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,29 @@
 // limitations under the License.
 
 /**
+ * Since addRandomGreeting and addFact are similar functions, I defined the greeting and facts arrays outside
+ * these functions so that I can make a more general function to print out these statements. The arrays are defined below.
+ */
+const greetings =
+    ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+const facts = 
+    ['I am actually a brother of the Gamma Chi chapter of the Sigma Alpha Mu Fraternity.','I have practiced playing both the Bass guitar and Ocarina.','I am studying Japanese and can speak it at an intermediate level (ぼくは日本に留学したい).','I never learned how to ride a bike.'];
+
+/**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
+function addRandomText(arr) {
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const greeting = arr[Math.floor(Math.random() * arr.length)];
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+function swapHidden(id1,id2) {
+    const toBeHidden = document.getElementById(id1);
+    const toBeRevealed = document.getElementById(id2);
+    toBeRevealed.style.display = 'inline-block';
+    toBeHidden.style.display = 'none';
 }
